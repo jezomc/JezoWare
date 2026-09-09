e22480817203bc8ccbf0d1d545987368a0912d6b
jezomc <aminehadjras36@gmail.com>
2026-09-09 11:53:57 +0100

    added a keybinds menu

--- JezoWare/Global.gd
+++ JezoWare/Global.gd
@@ -1,4 +1,8 @@
 extends Node
 
-var minigames_done = 0 #track how many minigames done
-var hearts = 5 #track how many lives left, also effecting garlic appearing
+var minigames_done = 0
+var hearts = 3
+
+func _process(delta: float) -> void:
+	if hearts <= 0:
+		get_tree().change_scene_to_file("res://scenes/lost_screen.tscn")
--- JezoWare/scenes/title_screen.gd
+++ JezoWare/scenes/title_screen.gd
@@ -14,6 +14,8 @@ func _process(delta: float) -> void:
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/timer_screen.tscn")
 
+func _on_keybinds_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/keybinds.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
