13eae170757a7c87d6a395b8fc3d9bd39006cf50
jezomc <aminehadjras36@gmail.com>
2026-09-10 15:08:44 +0100

    fixed some bugs.

--- JezoWare/scenes/minigame_1.gd
+++ JezoWare/scenes/minigame_1.gd
@@ -29,7 +29,7 @@ func _process(delta: float) -> void: # running every frame brochacho
 	if timer_end: # if the timer does end...
 		Global.minigames_done -=1 #go back a minigame
 		Global.hearts -= 1 # lose ur lives
-		SceneTransition.change_scene_to_file("res://scenes/timer_screen.tscn")# back to intermission
+		get_tree().change_scene_to_file("res://scenes/timer_screen.tscn")# back to intermission
 		
 
 func snowball_collect() -> void: # cool function that you connect to those garlics
